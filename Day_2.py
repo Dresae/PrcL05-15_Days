eee06cb221b052ed754aed0cf3cc6ea2b20daead
lvdev <lov3r.dev@gmail.com>
2024-11-26 13:56:15 +0000

    day 2 added

---
+++ Day_2.py
@@ -0,0 +1,13 @@
+"""Day 2 Challenge - Content
+================================
+1. Given a list of numbers, find the sum and average.
+2. Create a program that takes a temperature in Farenheit and converts it tp celsius
+3. Implement a program that checks if a given string is a palindrome
+4. Create a funtion that reverse a given string
+5. Given a list of names, concatenate them into a single string separated by dashes
+6. Write a Python program that check if a given string is a pangram(contains  all letter of the alphabet)
+7. calculate the area and circle given its radius
+8. Implement a program that converts a given number of minutes into hours and minutes
+9. Create a function to to count the number of vowels in a given string
+10. Write a program to check if a number is prime
+"""
