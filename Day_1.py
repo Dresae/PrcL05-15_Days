995f4ea02ee5e83b0ae88f813431278cf93fad10
lvdev <lov3r.dev@gmail.com>
2024-11-26 13:55:28 +0000

    day1 task 4 to 7 update

--- Day_1.py
+++ Day_1.py
@@ -38,3 +38,51 @@ if mod == 0:
     print(num, "is even")
 else:
     print(num, "is odd")
+
+
+# -------------------------------------------------------------------
+# 4. Given a list of numbers, find the max and min values in the list.
+
+numbers = [13, 64, 24, 10, 74, 97, 50, 52, 13, 24, 68, 85, 61, 69, 45, 43, 20, 90, 83, 4]
+
+max_value = max(numbers)
+min_value = min(numbers)
+
+print("The maximum value in the list is:", max_value)
+print("The minimum value in the list is:", min_value)
+
+
+# -------------------------------------------------------------------
+# 5. Create a function that check if given string is a palindrome
+def is_palindrome(s):
+    return s == s[::-1]
+
+input_string = input("Enter a string: ")
+if is_palindrome(input_string):
+    print("The string is a palindrome.")
+else:
+    print("The string is not a palindrome.")
+
+
+# -------------------------------------------------------------------
+# 6. Calculate the compound interest for a given principal, interest rate, and time period
+
+principal = float(input("Enter the principal amount: "))
+rate = float(input("Enter the interest rate: "))
+time = float(input("Enter the time period in years: "))
+
+compound_interest = principal * (1 + rate / 100) ** time
+
+print(f"The compound interest is: {compound_interest}")
+print("The compound interest is: $" + str(round(compound_interest,3)))
+
+
+# -------------------------------------------------------------------
+# 7. Converts a given numbers of days  into years, weeks and days
+days = int(input("Enter the number of days: "))
+
+years = days // 365
+weeks = (days % 365) // 7   
+days = (days % 365) % 7
+
+print(f"Years calculated: {years}" + "\n" + f"Weeks calculated: {weeks}" + "\n" + f"Days calculated: {days}")
