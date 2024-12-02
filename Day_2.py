ec057ec24bf089c443c504c85a8a30b77716ddc6
lvdev <lov3r.dev@gmail.com>
2024-12-02 18:13:43 +0000

    day 3 started

--- Day_2.py
+++ Day_2.py
@@ -135,6 +135,10 @@ def is_prime(n):
     return True
 
 number = int(input("Enter a number: "))
+if is_prime(number):
+    print(f"{number} is a prime number.")
+else:
+    print(f"{number} is not a prime number.")
 
 
 
---
+++ Day_3.py
@@ -0,0 +1,19 @@
+"""Day 3 Challenge - Content Control Flow and Loops
+==========================
+1. Write a program that takes a number as input and checks if it is positive, negative, or zero.
+2. Create a loop that prints the first 10 even numbers
+3. Iimplement a  program that finds the largest and smallest number in a list of integers
+4. Write a program that checks if a given year is a leap year or not
+5. Given a list of integers, find all the even numbers  and store them in a new list
+6. Create a program that generates the Fibonaccia sequence up to a given number of terms
+7. Given a list of names, print all the names starting with the letter 'A'
+8. Implement a program that prints the multiplication table of a given number
+9. Create a loop that prints all the prime numbers between 1 anf 100
+10. Given a list of words, count the number of words with more than 5 characters
+11. Calculate the sum of digits of a given number
+
+
+
+
+
+"""
