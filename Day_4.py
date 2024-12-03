942936bd6f619907254c9c9e24fda4fff17f23a4
lvdev <lov3r.dev@gmail.com>
2024-12-03 15:26:36 +0000

    day 4 started

---
+++ Day_4.py
@@ -0,0 +1,17 @@
+"""Day 3 Challenge - Function
+==========================
+1. Given a list of numbers, create a function to find the sum of all pisitive numbers
+2. Write a Python function that checks if a given string is a palindrome
+3. Implemenet a function to calculate the factorial of a number using recursion
+4. Create a function that finds the square of each element in a given list
+5. Write a function that checks if a number is even or odd and return "Even" or "Odd" accordingly.
+6. Calculate the area of a triangle given its base and height using a function
+7. Create a function that takes a list of strings and returns the list sorted alphabetically
+8. Write a function that takes two lists and return their intersection(common elements)
+9. Implement a function that checks if a given year is leap or not
+10. Create a function that takes a number as inut and prints its multiplication table
+11. Write a function to calculate the area of a circle given its radius
+12. Create a function to check if a given number is prime
+13. Implement a function that reverses a given string
+
+"""
