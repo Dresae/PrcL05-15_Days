7d7c25dcfbd92ab5f70a3ab6d0eaeafbce037a85
lvdev <lov3r.dev@gmail.com>
2024-12-05 12:42:02 +0000

    day 6 ch 1 to 5 done

--- Day_6.py
+++ Day_6.py
@@ -17,4 +17,57 @@
 """
 
 # -------------------------------------------------------------------
-# 1.
+# 1. Given two lists of numbers, concatenate them into a single list
+
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+
+concatenated_list = list1 + list2
+print(concatenated_list)
+
+# -------------------------------------------------------------------
+# 2. Write a program that finds the largest and smallest number in a list
+
+numbers = [13, 64, 24, 10, 74, 97, 50, 52, 13, 24, 68, 85, 61, 69, 45, 43, 20, 90, 83, 4]
+largest = max(numbers)
+smallest = min(numbers)
+
+print("The largest number in the list is:", largest)
+print("The smallest number in the list is:", smallest)
+
+# -------------------------------------------------------------------
+# 3. Implement a function that takes a list of numbers and returns a new list with squared values
+
+numbers = [1, 2, 3, 4, 5]
+
+def square_list(numbers):
+    squared = [] # empty list to store the new values
+    for num in numbers:
+        squared.append(num ** 2) # .append method is used tto add new values to the empty list
+    return squared
+
+print(square_list(numbers))
+
+# -------------------------------------------------------------------
+# 4. Create a program that finds the common elements between two lists and store them into a new list  
+list1 = [1, 2, 3, 4, 5]
+list2 = [4, 5, 6, 7, 8]
+
+common_elements = [value for value in list1 if value in list2] # This list comprehension check for common elements
+print(common_elements)
+
+# -------------------------------------------------------------------
+# 5. Given a list of words, find a word with the maximun length and its length
+
+words = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew"]
+
+max_length = 0 # this variable is used to  initialize the loop
+max_word = ""
+
+for word in words:
+    if len(word) > max_length: # here the loop iterate over each word in the list until it stops when finding the one with maximun lenght
+        max_length = len(word) # the variable value is update on each loop iteration
+        max_word = word
+
+print("The word with the maximum length is:", max_word)
+print("The length of the word is:", max_length)
---
+++ Day_7.py
@@ -0,0 +1,6 @@
+"""Day 7 Dictionaries and Sets
+==========================
+1. Given two dictionaries, merge them into a single dictionary
+
+
+"""
