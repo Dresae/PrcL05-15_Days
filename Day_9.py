b2fc407211dfb2d0fd5994ab6ad46934d72eef82
lvdev <lov3r.dev@gmail.com>
2024-12-17 21:06:34 -0700

    day9 ch 11 done

--- Day_9.py
+++ Day_9.py
@@ -272,3 +272,29 @@ print(movie1)
 print(movie2)
 print(movie3)
 '''
+
+# -------------------------------------------------------------------
+
+# 11. Create a class to represent a basic  calculator with add, substract, multiply and divide methods.
+class Calculator:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def add(self):
+        return self.num1 + self.num2
+
+    def subtract(self):
+        return self.num1 - self.num2
+
+    def multiply(self):
+        return self.num1 * self.num2
+
+    def divide(self):
+        return self.num1 / self.num2
+
+calculator = Calculator(10, 5)
+print(calculator.add())
+print(calculator.subtract())
+print(calculator.multiply())
+print(calculator.divide())
