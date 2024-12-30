9ca5a2054da87bb05b57aab16f77c8b0a76c7375
Dresae <94129538+Dresae@users.noreply.github.com>
2024-12-29 19:10:19 -0700

    day11 started

---
+++ Day_11.py
@@ -0,0 +1,19 @@
+"""Day 11 Introduction to Numpy for Numerical Computing
+==========================
+
+
+1. Given a list of numbers, create a NUmpy array and find the sum and products of its elements
+2. Implement a program that generates a Numpy array and reshapes it into a shape of 3x3 matrix
+3. Write a python program that uses NumPy to find the mean, median and standard deviation of a given dataset
+4. Create a function that takes a list of numbers and returns a NumPy array and sort it in ascending order
+5. Given a list of lists, create a 2D NumPy array and find the sum of elements in each row and column
+6. Implement a program that generates a NumPy array and finds the maximun and minimum value
+7. Write a function that takes a Numpy array and returns a new array and with all elements squared
+8. Given a NUmpy array, calculate the dot product with the array with itself
+9. Create a program that uses Numpy to calculate the inverse of a 2X2 matrix
+10. Implement a function that takes a Numpy array and returns the transpose of the array 
+11. Create a Numpy array for a python list and perform basic operations like addition, subtraction, multiplication and division
+12. Generate a Numpy array with random numbers and calculate its mean, median and standard deviation
+13. Create a Numpy array and reshape it into a different shape  
+
+"""
