c71298dfecacb67de7a43b8cd8fecaaed5e88201
Dresae <94129538+Dresae@users.noreply.github.com>
2024-12-29 20:20:59 -0700

    ch5-8

--- Day_11.py
+++ Day_11.py
@@ -6,8 +6,8 @@
 4. Create a function that takes a list of numbers and returns a NumPy array and sort it in ascending order
 5. Given a list of lists, create a 2D NumPy array and find the sum of elements in each row and column
 6. Implement a program that generates a NumPy array and finds the maximun and minimum value
-7. Write a function that takes a Numpy array and returns a new array and with all elements squared
-8. Given a NUmpy array, calculate the dot product with the array with itself
+7. Write a function that takes a Numpy array and returns a new array with all elements squared
+8. Given a Numpy array, calculate the dot product with the array with itself
 9. Create a program that uses Numpy to calculate the inverse of a 2X2 matrix
 10. Implement a function that takes a Numpy array and returns the transpose of the array 
 11. Create a Numpy array for a python list and perform basic operations like addition, subtraction, multiplication and division
@@ -67,11 +67,56 @@ print("The standard deviation of the dataset is:", data_std)
 # -------------------------------------------------------------------
 
 # 4. Create a function that takes a list of numbers and returns a NumPy array and sort it in ascending order
+"""
+import numpy as np
+list_1 = [325, 5421, 6589, 758, 258, 2368, 456, 789, 123, 4567]
+list_1_array = np.sort(np.array(list_1))
+
+print("The sorted array is:", list_1_array)
+"""
+# -------------------------------------------------------------------
+
+# 5. Given a list of lists, create a 2D NumPy array and find the sum of elements in each row and column
+"""
+import numpy as np
+list_1 = [[135, 82, 215], [54, 32, 548], [59, 25, 932], [65, 32, 456], [12, 23, 45]]
+list_1_array = np.array(list_1)
+
+print("The 2D array is:", "\n", list_1_array, "\n")
+
+print("The sum of elements in each column is:", list_1_array.sum(axis=0)) # axis=0 is used for operations with columns
+print("The sum of elements in each row is:", list_1_array.sum(axis=1))
+"""
+# -------------------------------------------------------------------
+
+# 6. Implement a program that generates a NumPy array and finds the maximun and minimum value
+"""
+import numpy as np
+my_array = np.array([135, 82, 215, 54, 32, 548, 59, 25, 932, 65, 32, 456, 12, 23, 45])
+max_value = np.max(my_array)
+min_value = np.min(my_array)
+
+print("The maximum value is:", max_value)
+print("The minimum value is:", min_value)
+"""
+
+# -------------------------------------------------------------------
+
+# 7. Write a function that takes a Numpy array and returns a new array with all elements squared
+"""
+import numpy as np
+my_array = np.array([3, 4, 5, 6, 7, 8, 9])
+squared_array = my_array**2
+
+print("Original array is:", my_array)
+print("Squared array is:", squared_array)
+"""
+# -------------------------------------------------------------------
+
+# 8. Given a Numpy array, calculate the dot product with the array with itself
 
 import numpy as np
-list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+my_array = np.array([3, 4, 5, 6, 7, 8, 9])
+dot_product = np.dot(my_array, my_array)
 
-list_1_array = np.array(list_1)
-sorted_array = np.sort(list_1_array)
-
-print("The sorted array is:", sorted_array)
+print("The dot product is:", dot_product)
