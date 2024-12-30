f1a3b1cf59738f07a1916f225f2989a58eca7d81
Dresae <94129538+Dresae@users.noreply.github.com>
2024-12-29 19:54:01 -0700

    day11 ch1-4

--- Day_11.py
+++ Day_11.py
@@ -1,8 +1,6 @@
 """Day 11 Introduction to Numpy for Numerical Computing
 ==========================
-
-
-1. Given a list of numbers, create a NUmpy array and find the sum and products of its elements
+1. Given a list of numbers, create a Numpy array and find the sum and products of its elements
 2. Implement a program that generates a Numpy array and reshapes it into a shape of 3x3 matrix
 3. Write a python program that uses NumPy to find the mean, median and standard deviation of a given dataset
 4. Create a function that takes a list of numbers and returns a NumPy array and sort it in ascending order
@@ -17,3 +15,63 @@
 13. Create a Numpy array and reshape it into a different shape  
 
 """
+
+# -------------------------------------------------------------------
+"""
+# 1. Given a list of numbers, create a Numpy array and find the sum and products of its elements
+import numpy as np
+list_1 = [1.25, 2.34, 3.45, 4.56, 5.54, 6.77, 7.89, 8.62, 9.59, 10.32]
+list_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+list_1_array = np.array(list_1) # convert the list into a numpy array using the 'np.array()' method
+list_2_array = np.array(list_2)
+
+sum_1 = np.round(list_1_array.sum(), decimals=2)    # the 'sum()' method is used to calculate the sum of its elements
+product_1 = np.round(list_1_array.prod(), decimals=2) # using round method to round off the decimals
+
+sum_2 = list_2_array.sum()
+product_2 = list_2_array.prod()
+
+
+print("The sum of list1 is:", sum_1)
+print("The product of list1 is:", product_1, "\n")
+print("The sum of list2 is:", sum_2)
+print("The product of list2 is:", product_2)
+"""
+
+# -------------------------------------------------------------------
+# 2. Implement a program that generates a Numpy array and reshapes it into a shape of 3x3 matrix
+"""
+import numpy as np
+array = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+reshaped_array = array.reshape(3, 3)
+print(reshaped_array)
+"""
+
+# -------------------------------------------------------------------
+
+# 3. Write a python program that uses NumPy to find the mean, median and standard deviation of a given dataset
+"""
+import numpy as np
+dataset = [1.25, 2.845, 3.546, 4.221, 5.75, 6.36, 7.64, 8, 9.89, 10]
+ 
+data_mean = np.round(np.mean(dataset), decimals=3) 
+data_median = np.round(np.median(dataset), decimals=3) 
+data_std = np.round(np.std(dataset), decimals=3)     
+
+print("The mean of the dataset is:", data_mean)
+print("The median of the dataset is:", data_median)
+print("The standard deviation of the dataset is:", data_std)
+"""
+
+# -------------------------------------------------------------------
+
+# 4. Create a function that takes a list of numbers and returns a NumPy array and sort it in ascending order
+
+import numpy as np
+list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+list_1_array = np.array(list_1)
+sorted_array = np.sort(list_1_array)
+
+print("The sorted array is:", sorted_array)
