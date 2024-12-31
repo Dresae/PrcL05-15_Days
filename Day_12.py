2c6622de4a27077c1c384dc903eedd630ccb545f
Dresae <94129538+Dresae@users.noreply.github.com>
2024-12-31 06:51:24 -0700

    day12 started

---
+++ Day_12.py
@@ -0,0 +1,18 @@
+""" Day 12 Introduction to Pandas for data manipulation and analysis
+==========================
+
+1. Given a CSV file with student details, read it into a Pandas dataframe and find the average age of the students.
+2. Implement a program that generates a Pandas series with dates and filter it to get dates in a specific range.
+3. Write a Python program that uses Pandas to read a CSV file and find maximun and minimum values in each column.
+4. Create a function that takes a Pandas dataframe and returns a new dataframe with rows sorted in ascending order.
+5. Given a Pandas dataframe, filter the rows to include only the rows where a speficic column meets a condition
+6. Implement  a program that reads a CSV file into a Pandas dataframe and finds the sum of a specific column
+7. Write a function that takes a Pandas dataframe and adds a new calculated column to the dataframe
+8. Given a Pandas dataframe, group the data by a specific column and calculate the mean of another column
+9. Create a program that reads a JSON file to a Pandas dataframe and extracts specific information from it
+10. Implement a function that takes a Pandas dataframe and returns the transpose of the dataframe
+11. Create a Pandas series from a list and perform basic operations like filtering, sorting,etc
+12. Read a CSV file into a Pandas dataframe and perform basic data manipulation operations
+13. Create a Pandas dataframe from a dictionary and perform filtering and grouping operations
+
+"""
